25e84fbe1ed942685ce3ccd82ba9567a09c04d4d
Draguve <ritwikpatil1998@gmail.com>
2022-12-30 03:41:52 +0530

    fixed json file gen

--- fetch-urls.mjs
+++ fetch-urls.mjs
@@ -1,4 +1,9 @@
 import url from 'node:url';
+import * as fs from 'fs';
+import pLimit from 'p-limit';
+
+// Example Concurrency of 3 promise at once
+const limit = pLimit(15);
 
 
 function paramsToObject(entries) {
@@ -9,12 +14,13 @@ function paramsToObject(entries) {
     return result;
 }
 
-for(let i = 0; i < 3; i++) {
+async function getItemsAtIndex(index){
     let test_url = "https://ignition4.customsforge.com/?draw=10&columns%5B0%5D%5Bdata%5D=addBtn&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&columns%5B1%5D%5Bdata%5D=artistName&columns%5B1%5D%5Bname%5D=artist.name&columns%5B2%5D%5Bdata%5D=titleName&columns%5B2%5D%5Bname%5D=title&columns%5B3%5D%5Bdata%5D=albumName&columns%5B3%5D%5Bname%5D=album&columns%5B4%5D%5Bdata%5D=year&columns%5B5%5D%5Bdata%5D=duration&columns%5B5%5D%5Borderable%5D=false&columns%5B6%5D%5Bdata%5D=tunings&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&columns%5B7%5D%5Bdata%5D=version&columns%5B7%5D%5Bsearchable%5D=false&columns%5B7%5D%5Borderable%5D=false&columns%5B8%5D%5Bdata%5D=memberName&columns%5B8%5D%5Bname%5D=author.name&columns%5B9%5D%5Bdata%5D=created_at&columns%5B9%5D%5Bsearchable%5D=false&columns%5B10%5D%5Bdata%5D=updated_at&columns%5B10%5D%5Bsearchable%5D=false&columns%5B11%5D%5Bdata%5D=downloads&columns%5B11%5D%5Bsearchable%5D=false&columns%5B12%5D%5Bdata%5D=parts&columns%5B12%5D%5Borderable%5D=false&columns%5B13%5D%5Bdata%5D=platforms&columns%5B14%5D%5Bdata%5D=file_pc_link&columns%5B14%5D%5Bsearchable%5D=false&columns%5B15%5D%5Bdata%5D=file_mac_link&columns%5B15%5D%5Bsearchable%5D=false&columns%5B16%5D%5Bdata%5D=artist.name&columns%5B17%5D%5Bdata%5D=title&columns%5B18%5D%5Bdata%5D=album&columns%5B19%5D%5Bdata%5D=author.name&columns%5B20%5D%5Bdata%5D=discussionID&order%5B0%5D%5Bcolumn%5D=10&order%5B0%5D%5Bdir%5D=desc&start=0&length=50&search%5Bvalue%5D=&table_filter_artist=&table_filter_album=&filter_title=&filter_album=&filter_preferred_platform=pc&filter_start_year=&filter_end_year=&filter_preferred=&filter_lovedCreators=&filter_official=&filter_rsplus=&filter_disable=&filter_notdisabled=&filter_hidden=&_=1672312450448"
     const myURL = url.parse(test_url);
     let newSearchParams = new URLSearchParams(myURL.search);
-    newSearchParams.set("start",(i*50).toString());
-    let data = fetch(test_url.substring(0,test_url.indexOf("?"))+"?"+newSearchParams.toString(), {
+    newSearchParams.set("start",(index*50).toString());
+    let data;
+    data = await fetch(test_url.substring(0, test_url.indexOf("?")) + "?" + newSearchParams.toString(), {
         "headers": {
             "accept": "application/json, text/javascript, */*; q=0.01",
             "accept-language": "en-US,en;q=0.9",
@@ -32,7 +38,31 @@ for(let i = 0; i < 3; i++) {
         },
         "body": null,
         "method": "GET"
-    }).then((response) => response.json())
-        .then((data) => console.log(data));
+    });
+    data = await data.json();
+    console.log(index);
+    if("data" in data)
+        return data["data"];
+    return []
 }
 
+let all_data = []
+var promises = [];
+for(let i = 0; i < 1218; i++) {
+    const promise = limit(() => getItemsAtIndex(i));
+    promises.push(promise)
+}
+const datas = await Promise.all(promises);
+for(let i=0;i<datas.length;i++){
+    for(let j=0;j<=datas[i].length;j++){
+        if(datas[i][j] !== undefined){
+            all_data.push(datas[i][j])
+        }
+    }
+}
+
+fs.writeFile("links.json", JSON.stringify(all_data), function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
