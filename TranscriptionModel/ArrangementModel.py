de532da880a4dbe82886be713f1e523f04ede683
Draguve <ritwikpatil1998@gmail.com>
2023-06-08 19:16:26 +0530

    something

--- TranscriptionModel/ArrangementModel.py
+++ TranscriptionModel/ArrangementModel.py
@@ -17,11 +17,19 @@ from pytorch_lightning import LightningModule
 from TranscriptionDataset import ArrangementDataset
 
 
+class Weighted_MSELoss(nn.Module):
+    def forward(self, inputs, targets, weights):
+        return (((inputs - targets) ** 2) * weights).mean()
+
+
 class ArrangementModel(LightningModule):
-    def __init__(self, arrangement_size):
+    def __init__(self, arrangement_size, class_weight_index):
         # self.count=0
         super().__init__()
-        self.acc = torch.tensor([0])
+        # self.acc = torch.tensor([0])
+        self.class_weight_index = class_weight_index
+        self.lossFunction = Weighted_MSELoss()
+
         self.conv1 = nn.Conv1d(2, 35, kernel_size=80, stride=16)
         self.bn1 = nn.BatchNorm1d(35)
         self.pool1 = nn.MaxPool1d(4)
@@ -37,9 +45,10 @@ class ArrangementModel(LightningModule):
         # self.fcTuning = nn.Linear(70, tuning_size)
         # self.fcCapo = nn.Linear(70, capo_size)
         self.flattener = nn.Flatten(1)
-        self.fcArrangement = nn.Linear(70 * 233,1000)
-        self.linear2 = nn.Linear(1000,100)
-        self.linear3 = nn.Linear(100,arrangement_size)
+        self.fcArrangement = nn.Linear(70 * 233, 1000)
+        self.linear2 = nn.Linear(1000, 100)
+        self.linear3 = nn.Linear(100, arrangement_size)
+        self.sigmoid = nn.Sigmoid()
         self.save_hyperparameters()
 
     def forward(self, x):
@@ -66,7 +75,7 @@ class ArrangementModel(LightningModule):
         x = self.linear3(x)
         # return F.log_softmax(tuning, dim=2), F.log_softmax(capo, dim=2), F.log_softmax(arrangement, dim=2)
 
-        return F.sigmoid(x)
+        return self.sigmoid(x)
 
     def configure_optimizers(self) -> Any:
         optimizer = torch.optim.Adam(self.parameters())
@@ -75,9 +84,13 @@ class ArrangementModel(LightningModule):
     def get_loss_and_acc(self, batch, batch_idx):
         section, tuning, arrangement = batch
         output = self(section).squeeze(1)
+        np_arr = arrangement.clone().detach().cpu().numpy().astype(bool)
+        ids = np.packbits(np_arr, bitorder="little", axis=1)
+        weights = torch.from_numpy(self.class_weight_index[ids]).to(output.device)
+        # print(output.device,arrangement.device,weights.device)
         # print(f"{self.count} {section.shape} {tuning.shape} {capo.shape} {arrangement.shape} {arrangement.squeeze(1).shape} {torch.argmax(arrangement.squeeze(1), dim=1).shape} {output.shape}")
         # self.count+=1
-        loss = F.mse_loss(output, arrangement.squeeze(1))
+        loss = self.lossFunction(output, arrangement.squeeze(1), weights)
         return loss
 
     def training_step(self, batch, batch_idx):
@@ -97,6 +110,21 @@ class ArrangementModel(LightningModule):
 
 
 if __name__ == '__main__':
+    weights = np.array([0.00000000e+00, 3.66532629e-01, 0.00000000e+00, 3.25338050e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 8.21091270e+01,
+                        2.70406430e+00, 1.06886416e-01, 0.00000000e+00, 6.52893475e-01,
+                        0.00000000e+00, 1.72429167e+03, 0.00000000e+00, 1.50374273e+01,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        1.39807432e+01, 1.00191265e+00, 0.00000000e+00, 1.96239568e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 6.89716667e+01,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        0.00000000e+00, 1.72429167e+03, 0.00000000e+00, 0.00000000e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+                        1.72429167e+02, 4.97391827e+01, 0.00000000e+00, 1.29321875e+02])
     SAMPLE_RATE = 16000
     dataset = "../Trainsets/massive_test2.hdf5"
     # transform = ArrangementDataset.OneHotEncodeArrangement(dataset)
@@ -115,7 +143,8 @@ if __name__ == '__main__':
     print(len(next(dataiter)))
     print(f"{section.shape} {tuning.shape} {arrangement.shape}")
     test = ArrangementModel(
-        arrangement_size=6
+        arrangement_size=6,
+        class_weight_index=weights
     )
     output = test.forward(section)
     test.training_step(next(dataiter), 0)
