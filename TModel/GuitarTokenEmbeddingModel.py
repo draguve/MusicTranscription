e61543c267aa649c0d6de26b840c6d84c5cb07db
Draguve <ritwikpatil1998@gmail.com>
2023-09-22 14:07:14 +0530

    Init token embedding model

--- TModel/GuitarTokenEmbeddingModel.py
+++ TModel/GuitarTokenEmbeddingModel.py
@@ -3,26 +3,39 @@ import lightning.pytorch as pl
 
 
 class GuitarTokenEmbeddingModel(pl.LightningModule):
-    def __init__(self, context_size=2, embedding_size=512, vocab_size=None):
+    def __init__(self, embedding_size=512, vocab_size=None):
         super().__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_size)
         self.linear1 = nn.Linear(embedding_size, vocab_size)
+        self.activation = nn.LogSoftmax(dim=1)
         self.loss = nn.CrossEntropyLoss()
+        self.save_hyperparameters()
 
     def forward(self, inputs):
         lookup_embeds = self.embeddings(inputs)
-        embeds = lookup_embeds.sum(dim=0)
+        embeds = lookup_embeds.sum(dim=1)
         out = self.linear1(embeds)
-        out = nn.functional.log_softmax(out)
+        out = self.activation(out)
         return out
 
     def training_step(self, batch, batch_idx):
         embedding, target = batch
         log_prob = self.forward(embedding)
         loss = self.loss(log_prob, target)
-        # Logging to TensorBoard (if installed) by default
-        self.log("train_loss", loss)
-        return
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        logits = self.forward(x)
+        loss = self.loss(logits, y)
+        self.log('val_loss', loss, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.loss(logits, y)
+        self.log('test_loss', loss, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
--- TranscriptionDataset/GuitarTokenDataset.py
+++ TranscriptionDataset/GuitarTokenDataset.py
@@ -4,26 +4,43 @@ import torchdata
 import torchdata.datapipes.iter as tdi
 import h5py
 import json
+from torch.utils.data import DataLoader, get_worker_info
+
 from Tokenizer.loaderH5 import H5GuitarTokenizer
 
 
 class GuitarTokenDataset(torchdata.datapipes.iter.IterDataPipe):
     def __init__(self, datasetFile, contextSize=2):
-        self.h5file = h5py.File(datasetFile, "r")
-        data = json.loads(self.h5file.get("Meta").attrs["section_index"])
-        self.keys = np.array(list(data.keys()))
+        self.h5file = None
+        self.datasetFile = datasetFile
+        h5file = h5py.File(datasetFile, "r")
+        meta = h5file.get("Meta")
+        self.section_data = json.loads(meta.attrs["section_index"])
+        self.meta_data = {}
+        for key in meta.attrs.keys():
+            if "index" not in key:
+                self.meta_data[key] = meta.attrs[key]
         self.lengthOfDataset = 0
         self.contextSize = contextSize
-        for key in data:
-            self.lengthOfDataset += data[key]["numTokens"]
+        for key in self.section_data:
+            self.lengthOfDataset += self.section_data[key]["numTokens"]
+
+    def getVocabSize(self):
+        return self.meta_data["vocab_size"]
 
     def __iter__(self):
+        worker = get_worker_info()
+        self.keys = np.array(list(self.section_data.keys()))
+        if worker is not None:
+            split_size = len(self.keys) // worker.num_workers
+            self.keys = self.keys[worker.id * split_size:(worker.id + 1) * split_size]
+        h5file = h5py.File(self.datasetFile, "r")
         for key in range(self.keys.shape[0]):
-            sectionGroup = self.h5file[f"/Songs/{self.keys[key]}"]
+            sectionGroup = h5file[f"/Songs/{self.keys[key]}"]
             tokens = sectionGroup["tokens"][...]
             for i in range(self.contextSize, tokens.shape[0] - self.contextSize):
-                yield np.concatenate((tokens[i - self.contextSize:i], tokens[i + 1:i + self.contextSize + 1])), tokens[
-                    i]
+                yield (np.concatenate((tokens[i - self.contextSize:i], tokens[i + 1:i + self.contextSize + 1])),
+                       torch.tensor(tokens[i], dtype=torch.long))
 
     def __len__(self):
         return self.lengthOfDataset
@@ -31,21 +48,23 @@ class GuitarTokenDataset(torchdata.datapipes.iter.IterDataPipe):
 
 def getDataPipe(datasetLocation, contextSize=2, shuffleBufferSize=50000, prefetchSize=None,
                 pinMemory=False):
-    pipe = GuitarTokenDataset(datasetLocation, contextSize)
-    pipe = pipe.shuffle(buffer_size=shuffleBufferSize)
+    dataset = GuitarTokenDataset(datasetLocation, contextSize)
+    pipe = dataset.shuffle(buffer_size=shuffleBufferSize)
     if prefetchSize is not None:
         pipe = tdi.Prefetcher(pipe, prefetchSize)
     if pinMemory:
         pipe = tdi.PinMemory(pipe)
-    return pipe
+    return dataset, pipe
 
 
 def test():
     dataset = "../Trainsets/S_Tier_1695289757_mTokens1000_mNoS60.hdf5"
     # tokenizer = H5GuitarTokenizer(dataset)
-    pipe = getDataPipe(dataset, 10)
-    for i in pipe:
+    dataset, pipe = getDataPipe(dataset, 2)
+    train_dl = DataLoader(dataset=pipe, batch_size=64, num_workers=2)
+    for i in train_dl:
         print(i)
+        break
 
 
 if __name__ == '__main__':
