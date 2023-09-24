86771a86a319e3f6e0fb97c18a001fb258be9eba
Draguve <ritwikpatil1998@gmail.com>
2023-09-24 11:45:06 +0530

    Cleanup and somewhat better loader

--- TranscriptionDataset/TranscriptionDataset.py
+++ TranscriptionDataset/TranscriptionDataset.py
@@ -1,27 +1,51 @@
+import math
+
 import numpy as np
 import torch
 import torchdata
 import torchdata.datapipes.iter as tdi
 import h5py
 import json
+
+from torch.utils.data import get_worker_info, DataLoader
+from tqdm import tqdm
+
 from Tokenizer.loaderH5 import H5GuitarTokenizer
 
 
-class TranscriptionDataset(torchdata.datapipes.map.MapDataPipe):
-    def __init__(self, dataset_file):
-        self.h5file = h5py.File(dataset_file, "r")
-        data = json.loads(self.h5file.get("Meta").attrs["section_index"])
-        self.keys = np.array(list(data.keys()))
-        self.lengthOfDataset = len(self.keys)
+class TranscriptionDataset(torchdata.datapipes.iter.IterDataPipe):
+    def __init__(self, datasetFile, batchSize=10):
+        self.h5file = None
+        self.datasetFile = datasetFile
+        h5file = h5py.File(datasetFile, "r")
+        meta = h5file.get("Meta")
+        self.section_data = json.loads(meta.attrs["section_index"])
+        self.meta_data = {}
+        self.batchSize = batchSize
+        for key in meta.attrs.keys():
+            if "index" not in key:
+                self.meta_data[key] = meta.attrs[key]
+        self.lengthOfDataset = len(list(self.section_data.keys()))
 
-    def __getitem__(self, item):
-        sectionGroup = self.h5file[f"/Songs/{self.keys[item]}"]
-        mel = sectionGroup["mel"][...]
-        tokens = sectionGroup["tokens"][...]
-        return mel, tokens
+    def getVocabSize(self):
+        return self.meta_data["vocab_size"]
+
+    def __iter__(self):
+        worker = get_worker_info()
+        self.keys = np.array(list(self.section_data.keys()))
+        if worker is not None:
+            split_size = len(self.keys) // worker.num_workers
+            self.keys = self.keys[worker.id * split_size:(worker.id + 1) * split_size]
+        np.random.shuffle(self.keys)
+        h5file = h5py.File(self.datasetFile, "r")
+        for key in range(self.keys.shape[0]):
+            sectionGroup = h5file[f"/Songs/{self.keys[key]}"]
+            mel = sectionGroup["mel"][...]
+            tokens = sectionGroup["tokens"][...]
+            yield mel, tokens
 
     def __len__(self):
-        return self.lengthOfDataset
+        return math.ceil(self.lengthOfDataset / self.batchSize)+1
 
 
 def bucketBatcherSort(data):
@@ -48,35 +72,29 @@ def datasetCollateFn(all_data):
     return np.stack(all_mels), np.stack(all_src_masks), np.stack(all_tokens), np.stack(all_tgt_masks)
 
 
-def getDataPipe(datasetLocation, batchSize=10, shuffleBufferSize=1000, postBucketShuffle=100, prefetchSize=None,
-                pinMemory=False):
-    pipe = TranscriptionDataset(datasetLocation)
-    pipe = pipe.to_iter_datapipe().shuffle(buffer_size=shuffleBufferSize)
+def getDataPipe(datasetLocation, batchSize=10, prefetchSize=None, pinMemory=False):
+    dataset = TranscriptionDataset(datasetLocation, batchSize=batchSize)
     pipe = tdi.BucketBatcher(
-        pipe,
+        dataset,
         batch_size=batchSize,
         sort_key=bucketBatcherSort,
         use_in_batch_shuffle=True,
     )
-    pipe = pipe.shuffle(buffer_size=postBucketShuffle)
     pipe = tdi.Collator(pipe, collate_fn=datasetCollateFn)
     if prefetchSize is not None:
         pipe = tdi.Prefetcher(pipe, prefetchSize)
     if pinMemory:
         pipe = tdi.PinMemory(pipe)
-    return pipe
+    return dataset, pipe
 
 
 def test():
-    dataset = "../Trainsets/TokensOnly_1695456272_mTokens1000_mNoS60.hdf5"
-    tokenizer = H5GuitarTokenizer(dataset)
-    pipe = getDataPipe(dataset, 10)
-    for data in pipe:
-        print([i.shape for i in data])
-        for x in data[2]:
-            for y in x:
-                print(tokenizer.encoder.decode(y))
-        break
+    datasetLocation = "../Trainsets/S_Tier_1695428558_mTokens1000_mNoS60.hdf5"
+    tokenizer = H5GuitarTokenizer(datasetLocation)
+    dataset, pipe = getDataPipe(datasetLocation, 10)
+    train_dl = DataLoader(dataset=pipe, num_workers=4)
+    for i in tqdm(train_dl, total=len(dataset)):
+        continue
 
 
 if __name__ == '__main__':
